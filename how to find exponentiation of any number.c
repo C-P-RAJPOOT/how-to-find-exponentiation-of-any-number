69e7cde061301cee998ae4ea624572a0a1bd4f9f
C-P-RAJPOOT <97006020+C-P-RAJPOOT@users.noreply.github.com>
2022-01-16 16:15:21 +0530

    Add files via upload

---
+++ how to find exponentiation of any number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void main()
+{
+	
+	int i=2,j=3,k,l;
+	float a,b;
+	k=i/j*j;
+	l=j/i*i;
+	a=i/j*j;
+	b=j/i*i;
+	printf("k=%d",k);
+	printf("\nl=%d",l);
+	
+}
